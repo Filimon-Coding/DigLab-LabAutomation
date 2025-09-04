1a91a75dc768259563981bcfc66bcd68cbd6e754
Filimon-Coding <filimon.nuguse22@gmail.com>
2025-09-04 18:21:06 +0200

    Skal bSkal begynne med laste opp av pdf for scann part

---
+++ DigLab/backEnd/DigLabAPI/Migrations/20250904155912_Init.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DigLabAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    LabNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Personnummer = table.Column<string>(type: "TEXT", nullable: true),
+                    Date = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    Time = table.Column<TimeOnly>(type: "TEXT", nullable: false),
+                    DiagnosesJson = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAtUtc = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_LabNumber",
+                table: "Orders",
+                column: "LabNumber",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
