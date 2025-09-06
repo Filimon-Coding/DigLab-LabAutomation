ebd110c3f1518de1181834f3a7c0e3352c1b716c
Filimon-Coding <filimon.nuguse22@gmail.com>
2025-09-06 14:22:45 +0200

    Forbedret

--- DigLab/backEnd/DigLabAPI/obj/Debug/net8.0/DigLabAPI.AssemblyInfo.cs
+++ DigLab/backEnd/DigLabAPI/obj/Debug/net8.0/DigLabAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigLabAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a91a75dc768259563981bcfc66bcd68cbd6e754")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48acc5396677ff9f8d9b786d67b00e8392b0201f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigLabAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigLabAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
