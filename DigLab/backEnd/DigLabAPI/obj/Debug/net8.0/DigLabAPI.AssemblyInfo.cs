dbfd0128dfffbb701c150f0797445b4dacd5754a
Filimon-Coding <filimon.nuguse22@gmail.com>
2025-10-08 23:52:40 +0200

    analyze test.. need teseract

--- DigLab/backEnd/DigLabAPI/obj/Debug/net8.0/DigLabAPI.AssemblyInfo.cs
+++ DigLab/backEnd/DigLabAPI/obj/Debug/net8.0/DigLabAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigLabAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+794ac13c851cba431a53c5e19a5a91eb8ca0304c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+432df0d01df187b36cb64b58cf1aff4a7eb5678b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigLabAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigLabAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
