8f5438296b634af84ac857025368c740f6eb217e
Filimon-Coding <filimon.nuguse22@gmail.com>
2025-10-29 12:13:25 +0100

    sync test for git hub, før jeg starte på innlogging del av projektet

--- DigLab/backEnd/DigLabAPI/obj/Debug/net8.0/DigLabAPI.AssemblyInfo.cs
+++ DigLab/backEnd/DigLabAPI/obj/Debug/net8.0/DigLabAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigLabAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bd30816fa0eaa1062d31fafd398dd40e25dc385")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c55854179355f2cf9e951ee7318a6f88aba34ccc")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigLabAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigLabAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
